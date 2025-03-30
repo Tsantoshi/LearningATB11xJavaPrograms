dc5f3fc590068362a5be59a671c472877b46aee5
DELL <santoshimthakur@gmail.com>
2025-03-30 19:45:43 +0530

    28 March Task

---
+++ src/Tasks/PyramidPattern.java
@@ -0,0 +1,20 @@
+package Tasks;
+
+public class PyramidPattern {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 9; i++) {
+            if (i % 2 == 0) {
+                for (int j = 9; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <= i; k++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+}
+
