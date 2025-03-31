f793c0bb4e97978f2490b3309fcda9d6a92461c6
DELL <santoshimthakur@gmail.com>
2025-03-31 17:01:38 +0530

    31 March Task

---
+++ src/Tasks/Class_all_4typeMethods.java
@@ -0,0 +1,48 @@
+package Tasks;
+
+
+public class Class_all_4typeMethods {
+    public static void main(String[] args) {
+         Person p1 = new Person();
+
+         p1.greet();
+         p1.married();
+         p1.Person_details("Santoshi", "Thakur","Hyderabad");
+         p1.person_Salary(50000);
+
+    }
+}
+class Person{
+    String FirstName;
+    String LastName;
+    int age;
+    String address;
+    double salary;
+    boolean ismarried;
+
+
+    //1. without param without RT
+     void greet(){
+        System.out.println("Hi , How are you?");
+    }
+
+    //2. Without param with RT
+    boolean married(){
+        ismarried = true;
+        System.out.println("Is married " +ismarried);
+        return ismarried;
+    }
+
+   // 3.With Parameters and Without RT
+     void Person_details(String FirstName, String LastName, String address) {
+        System.out.println("FirstName: " +FirstName +" "+ "LastName: " + LastName +" "+ "Address: " +" "+ address);
+    }
+
+
+    // 4. With Parameters and With RT
+    double person_Salary(double salary){
+        System.out.println("Salary is " +salary);
+        return salary;
+    }
+}
+
